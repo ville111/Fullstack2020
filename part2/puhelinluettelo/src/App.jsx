8beafe2b10ee8ec5f9725d2633cc7f133993cc2a
Ville <ville.suonurmi@helsinki.fi>
2024-12-16 14:01:54 +0200

    udate

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from 'react'
+import axios from 'axios'
 import { useState } from 'react'
+import personService from './services/persons'
 
-const Person = ({name, number}) => (<p>{name} {number}</p>)
-const Persons = ({persons}) =>  ( persons.map(person => <Person key={person.name} name={person.name} number={person.number} />) )
+
+const Notification = ({ message }) => {
+
+  if (message === null) {
+    return null
+  }
+
+  const messageStyle = {
+    color: 'green',
+    fontStyle: 'bold',
+    fontSize: 16,
+    padding: 10,
+    border: '4px solid green',
+    borderRadius: 3
+  }
+  return (
+    <div style={messageStyle}>
+      {message} 
+    </div>
+  )
+}
+
+const Person = ({id, name, number,handleDeleteChange}) =>  (
+    <div>
+  <form>{id} {name} {number} <input type="button" value="delete" id={id} name={name} onClick={handleDeleteChange}/></form>
+ </div>
+)
+
+
+const Persons = ({persons,handleDeleteChange}) =>  ( persons.map(person => <Person id={person.id} key={person.name} name={person.name} number={person.number} handleDeleteChange={handleDeleteChange}/>) )
 
 const Inputrow = ({label, value, onChange}) => (
   <div>{label}: <input value={value} onChange={onChange} /></div>
@@ -19,13 +50,18 @@ const Personform = ({addPerson,newName, newNumber, handlePersonChange,handleNumb
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '0955555'
-     }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+    }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,7 +69,22 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
     else {
-      setPersons(persons.concat({ name: newName, number: newNumber })) 
+      const personObject = { name: newName, number: newNumber }
+      personService
+      .create(personObject)
+      .then(returnedPerson => {
+       
+        setNotificationMessage(
+          `Added '${newName}'`
+        ) 
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 2000)
+
+        setPersons(persons.concat(returnedPerson))
+      
+      })
+     
     }
     setNewName('')
     setNewNumber('')
@@ -42,12 +93,32 @@ const App = () => {
 
   const handlePersonChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleDeleteChange = (event) => {
+   
+    if (window.confirm(`Delete ${event.target.name}?`)) {
+      personService
+      .remove(event.target.id)
+      .then(deletedPerson => {
+          console.log(`${event.target.name} deleted`)
+          setNotificationMessage(
+            `Deleted '${event.target.name}'`
+          ) 
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 2000)
+  
+          setPersons(persons.filter(person => person.id != deletedPerson.id))
+      })
+     
+    }
+  }
   
 
   return (
     <div>
       <h2>Phonebook</h2>
 
+     <Notification message={notificationMessage} />
      <Personform addPerson={addPerson}
       newName={newName}
       newNumber={newNumber} 
@@ -57,7 +128,8 @@ const App = () => {
 
       <h2>Numbers</h2>
       
-      <Persons persons={persons} />
+      <Persons persons={persons} 
+      handleDeleteChange={handleDeleteChange} />
       
     </div>
   )
